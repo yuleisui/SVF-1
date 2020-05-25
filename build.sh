d0f2adee92cf71bea0bf576a603c091551a242a7
charles <596147986@qq.com>
2020-05-25 21:41:24 +1000

    ubuntu_only_1

--- build.sh
+++ build.sh
@@ -1,46 +1,88 @@
-#  !bash
-# type './build.sh'  for release build
-# type './build.sh debug'  for debug build
+# !bash
+ROOT_PATH=$(pwd)
+sysOS=`uname -s`
+mac_llvm_source="https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-apple-darwin.tar.xz"
+ubuntu_llvm_source="https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz"
+mac_z3_source="https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-osx-10.14.6.zip"
+ubuntu_z3_source="https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-ubuntu-16.04.zip"
 
-#########
-# Please change the following home directories of your LLVM builds
-########
-LLVM_DIR_RELEASE=/Users/yulei/Documents/llvm-10.0.0/llvm-10.0.0.obj
-LLVM_DIR_DEBUG=/Users/yulei/Documents/llvm-10.0.0/llvm-10.0.0.dbg
-
-if [[ $1 == 'debug' ]]
-then
-BuildTY='Debug'
-	if [ -z "$LLVM_DIR" ]
-	then
-	echo "\$LLVM_DIR is not configured, using the default one"
-	export LLVM_DIR=$LLVM_DIR_DEBUG
-	fi
+echo "check LLVM_DIR and Z3_PACKAGE!"
+if [ $LLVM_DIR ]
+then echo "LLVM exist!!!"
 else
-BuildTY='Release'
-	if [ -z "$LLVM_DIR" ]
-	then
-	echo "\$LLVM_DIR is not configured, using the default one"
-	export LLVM_DIR=$LLVM_DIR_RELEASE
-	fi
+    if [ -x "LLVM_source.tar.xz" ] ; then
+        if [ -d "LLVM_source" ] ; then
+            rm -rf ./'LLVM_source'
+            mkdir ./LLVM_source && tar -xvf "LLVM_source.tar.xz" -C ./LLVM_source --strip-components 1
+            export LLVM_DIR=$ROOT_PATH"/"$LLVM_source
+        else
+            mkdir ./LLVM_source && tar -xvf "LLVM_source.tar.xz" -C ./LLVM_source --strip-components 1
+            export LLVM_DIR=$ROOT_PATH"/"$LLVM_source
+        fi
+    else
+        if [ $sysOS == "Darwin" ] ; then
+            echo 'macOS '
+            wget -c $mac_llvm_source -O LLVM_source.tar.xz
+            mkdir ./LLVM_source && tar -xvf "LLVM_source.tar.xz" -C ./LLVM_source --strip-components 1
+            export LLVM_DIR=$ROOT_PATH"/LLVM_source"
+        elif [ $sysOS == "Linux" ] ; then
+            echo 'ubuntu'
+            wget -c $ubuntu_llvm_source -O LLVM_source.tar.xz
+            mkdir ./LLVM_source && tar -xvf "LLVM_source.tar.xz" -C ./LLVM_source --strip-components 1
+            export LLVM_DIR=$ROOT_PATH"/LLVM_source"
+        fi
+    fi
 fi
-echo "LLVM_DIR =" $LLVM_DIR
-
-##download the test cases
+if [ $Z3_DIR ]
+then
+    echo 'Z3 package exist!!!'
+else
+    if [ -x "Z3_source.zip" ] ; then
+        if [ -d "Z3_source" ] ; then
+            rm -rf ./'Z3_source'
+            unzip "Z3_source.zip" && mv ./z3-* ./Z3_source
+            export Z3_DIR=$ROOT_PATH"/Z3_source"
+        else
+            unzip "Z3_source.zip" && mv ./z3-* ./Z3_source
+            export Z3_DIR=$ROOT_PATH"/Z3_source"
+        fi
+    else
+        if [ $sysOS == "Darwin" ] ; then
+            echo 'macOS '
+            wget -c $mac_z3_source -O Z3_source.zip
+            unzip "Z3_source.zip" && mv ./z3-* ./Z3_source
+            export Z3_DIR=$ROOT_PATH"/Z3_source"
+        elif [ $sysOS == "Linux" ] ; then
+            echo 'ubuntu'
+            wget -c $ubuntu_z3_source -O Z3_source.zip
+            unzip "Z3_source.zip" && mv ./z3-* ./Z3_source
+            export Z3_DIR=$ROOT_PATH"/Z3_source"
+        fi
+    fi
+fi
+#
+#
+. ./setup.sh $LLVM_DIR
 if [ -d 'Test-Suite' ] ; then
 echo 'Test-Suite exists！'
 else
 rm -rf ./'Test-Suite'
 echo 'Download Test-Suite'
-git clone "https://github.com/SVF-tools/Test-Suite.git"
+git clone "https://github.com/JasonZhongZexin/Test-Suite.git"
 cd ./'Test-Suite'
 ./generate_bc.sh
 cd ..
 fi
+echo "check cmake !!"
+cmake --version
+if [[ $1 == 'debug' ]]
+then
+BuildTY='Debug'
+else
+BuildTY='Release'
+fi
 
-export PATH=$LLVM_DIR/bin:$PATH
 Build=$BuildTY'-build'
-
 rm -rf ./$Build
 mkdir ./$Build
 cd ./$Build
@@ -48,23 +90,14 @@ cd ./$Build
 ## start building SVF
 if [[ $1 == 'debug' ]]
 then
+echo "check svf mode for debug"
 cmake -D CMAKE_BUILD_TYPE:STRING=Debug ../
 else
+echo "check svf mode for release"
 cmake ../
 fi
 make -j4
 
 ctest
-
-## set up environment variables of SVF
 cd ../
-if [[ $1 == 'debug' ]]
-then
-. ./setup.sh debug
-else
-. ./setup.sh
-fi
-#########
-# Optionally, you can also specify a CXX_COMPILER and your $LLVM_HOME for your build
-# cmake -DCMAKE_CXX_COMPILER=$LLVM_DIR/bin/clang++ -DLLVM_DIR=$LLVM_DIR
-#########
+. ./setup.sh $LLVM_DIR
--- setup.sh
+++ setup.sh
@@ -1,20 +1,6 @@
 echo "Setting up environment for SVF"
 
-
-#########
-# Please change LLVM_DIR_RELEASE before using it
-########
-
-export LLVM_DIR_RELEASE=/home/ysui/llvm-9.0.0/llvm-9.0.0.obj
-
-if [ -z "$LLVM_DIR" ]
-then
-	echo "\$LLVM_DIR is not configured, using the default one"
-	export LLVM_DIR=$LLVM_DIR_RELEASE
-fi
-
-echo "LLVM_DIR =" $LLVM_DIR
-
+export LLVM_DIR=$1
 export PATH=$LLVM_DIR/bin:$PATH
 export LLVMOPT=$LLVM_DIR/bin/opt
 export CLANG=$LLVM_DIR/bin/clang
@@ -22,46 +8,4 @@ export CLANGCPP=$LLVM_DIR/bin/clang++
 export LLVMDIS=$LLVM_DIR/bin/llvm-dis
 export LLVMLLC=$LLVM_DIR/bin/llc
 
-##############astyle code formatting###############
-AstyleDir=/home/ysui/astyle/build/clang
-export PATH=$AstyleDir/bin:$PATH
 
-##############check what os we have
-PLATFORM='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-export PLATFORM='linux'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-export PLATFORM='darwin'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-export PLATFORM='freebsd'
-fi
-
-
-#########PATH FOR PTA##############                                                                 
-export SVF_HOME=`pwd`
-if [[ $1 == 'debug' ]]
-then
-PTAOBJTY='Debug'
-else
-PTAOBJTY='Release'
-fi
-Build=$PTAOBJTY'-build'
-export SVF_HOME=`pwd`
-export PTABIN=$SVF_HOME/$Build/bin
-export PTALIB=$SVF_HOME/$Build/lib
-export PTARTLIB=$SVF_HOME/lib/RuntimeLib
-export PATH=$PTABIN:$PATH
-
-export PTATEST=$SVF_HOME/PTABen
-export PTATESTSCRIPTS=$PTATEST/scripts
-export RUNSCRIPT=$PTATESTSCRIPTS/run.sh
-
-### for mac 10.10.1###
-rm -rf $PTALIB/liblib*
-for file in $(find $PTALIB -name "*.dylib")
-do
-    basefilename=`basename $file`
-    newfile=`echo $basefilename | sed s/lib/liblib/`
-    ln -s $PTALIB/$basefilename $PTALIB/$newfile
-done
